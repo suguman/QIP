a764ba0d127f36dc4107317f407c6441067e5ad2
suguman <sugumanb@gmail.com>
2017-11-14 23:56:40 -0600

    Added DSComparator files

---
+++ src/DSComparator.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : BA.cpp
+// Author      : Suguman Bansal
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Class for Discounted-sum Buchi comparator
+//============================================================================
+
+#include "common.h"
+#include "DSComparator.h"
+
+
+using namespace std;
+
+DS::DS(int df, int* difList, int alphanum){
+  
+  
+  
+}
+
+
+DS::~DS(){
+  //TODO
+}
---
+++ src/DSComparator.h
@@ -0,0 +1,33 @@
+/*
+ * DSComparator.h
+ *
+ * Created on: November 14, 2017
+ * Author: Suguman Bansal
+ */
+
+#ifndef DSCOMPARATOR_H
+#define DSCOMPARATOR_H
+
+
+
+#include "common.h"
+#include "Transition.h"
+
+using namespace std;
+
+class DS: public BA{
+
+ public:
+
+  DS(int df, int* difList, int alphanum);
+
+  virtual ~DS();
+  
+ private:
+ 
+  
+  
+};
+
+
+#endif 
